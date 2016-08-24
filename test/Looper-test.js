664f97666424f6dbda131d292f2095c75512763f
logiclogue <jordanpaullord@hotmail.co.uk>
2016-08-24 18:31:12 +0100

    Adds Looper test

---
+++ test/Looper-test.js
@@ -0,0 +1,27 @@
+var assert = require('chai').assert;
+var LoopFunctions = require('../src/LoopFunctions.js');
+var Looper = require('../src/Looper');
+
+
+describe('Looper', function () {
+    var loopFunctions = new LoopFunctions();
+    var looper = new Looper(loopFunctions);
+
+    loopFunctions.draw = function () {
+        return 'start';
+    };
+    
+    loopFunctions.update = function () {
+        return 'stop';
+    };
+
+    describe('#constructor(LoopFunctions)', function () {
+        it('should throw error if looper isn\'t passed', function () {
+            assert.throws(loopFunctions, ReferenceError);
+        });
+
+        it('should set #loopFunctions with passed in object', function () {
+            assert.equal(looper.loopFunctions, loopFunctions);
+        });
+    });
+});
